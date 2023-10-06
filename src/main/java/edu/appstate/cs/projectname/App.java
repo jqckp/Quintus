3d22b00d4855edeab635fcfc23321d78113f5181
Mark Hills <mhills@acm.org>
2023-10-05 22:30:50 -0500

    Initial commit

---
+++ src/main/java/edu/appstate/cs/projectname/App.java
@@ -0,0 +1,13 @@
+package edu.appstate.cs.projectname;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
