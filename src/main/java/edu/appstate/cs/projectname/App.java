280b79a9bab730dcdbef63aca0adee51b4e61e33
Jack <porterjp1@appstate.edu>
2023-10-25 11:28:29 -0400

    Jack testing

--- src/main/java/edu/appstate/cs/projectname/App.java
+++ src/main/java/edu/appstate/cs/projectname/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Jack is a bisnessman" );
+        System.out.println( "Jack is NOT a bisnessman" );
     }
 }
