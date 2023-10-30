216f286071d648339028e206bc0a082db491927e
Andrew Turpin <turpinad@appstate.edu>
2023-10-30 09:44:52 -0400

    Andrew Test

--- src/main/java/edu/appstate/cs/quintus/App.java
+++ src/main/java/edu/appstate/cs/quintus/App.java
@@ -6,12 +6,16 @@ package edu.appstate.cs.quintus;
  */
 public class App 
 {
-    public static int sum(int x, int y) {
+    public static int sum(int x, int y) 
+    {
+        //make sure all {} are on their own lines.
         return x + y;
     }
 
     public static void main( String[] args )
     {
         System.out.println( "Jack is NOT a bisnessman" );
+        System.out.println("This is to test committing- Andrew");
     }
+
 }
