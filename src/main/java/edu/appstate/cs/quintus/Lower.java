60efecb0fe6e74e6b8feaad0ee74e712d136f8f6
Andrew Turpin <turpinad@appstate.edu>
2023-11-02 14:29:54 -0400

    Lower Method

---
+++ src/main/java/edu/appstate/cs/quintus/Lower.java
@@ -0,0 +1,13 @@
+package edu.appstate.cs.quintus;
+
+/**
+ * This class contains the method for finding the lowest value
+ * between two doubles. 
+ * 
+ * @author Andrew Tuprin
+ * @version 11/2/2023
+ */
+public class Lower 
+{
+    
+}
