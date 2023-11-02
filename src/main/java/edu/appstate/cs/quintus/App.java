467bd5c737c12d4f3b13a6127c1a5b917a4d2d26
Oscar Silva-Santiago <oscarsilva032104@gmail.com>
2023-11-02 14:21:57 -0400

    Blank

--- src/main/java/edu/appstate/cs/quintus/App.java
+++ src/main/java/edu/appstate/cs/quintus/App.java
@@ -1,21 +1,8 @@
 package edu.appstate.cs.quintus;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static int sum(int x, int y) 
-    {
-        //make sure all {} are on their own lines.
-        return x + y;
-    }
-
-    public static void main( String[] args )
-    {
-        System.out.println( "Jack is NOT a bisnessman" );
-        System.out.println("This is to test committing- Andrew");
-    }
+   
 
 }
