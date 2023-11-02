b381c64786073ab007ed4e4fe9a0ecee4f7d87e1
Oscar Silva-Santiago <oscarsilva032104@gmail.com>
2023-11-02 14:32:58 -0400

    userInput METHOD

---
+++ src/main/java/edu/appstate/cs/quintus/userInput.java
@@ -0,0 +1,17 @@
+package edu.appstate.cs.quintus;
+
+/**
+ * A maze game.
+ * 
+ * @author Oscar Silva
+ * @version 11/2/2023
+ *
+ */
+
+public class userInput {
+    
+    public userInput()
+    {
+
+    }
+}
