642a7c64670049d4c848b1afe1142bd4d20a189c
Oscar Silva-Santiago <oscarsilva032104@gmail.com>
2023-11-09 14:50:48 -0500

    Create Flight Class
    New Flight class crreated for object in linked-list. Getter and setters
    created for class with constructors.

---
+++ src/main/java/edu/appstate/cs/quintus/Flight.java
@@ -0,0 +1,65 @@
+package edu.appstate.cs.quintus;
+
+public class Flight 
+{
+    private String startDate;
+    private String returnDate;
+    private String airline;
+    private double cost;
+
+    public Flight()
+    {
+        startDate = "";
+        returnDate = "";
+        airline = "";
+        cost = 0.0;
+    }
+
+    public Flight(String startDate, String returnDate, String airline, double cost)
+    {
+        setStartDate(startDate);
+        setReturnDate(returnDate);
+        setAirline(airline);
+        setCost(cost);
+    }
+
+    public void setStartDate(String startDate)
+    {
+        this.startDate = startDate;
+    }
+
+    public void setReturnDate(String returnDate)
+    {
+        this.returnDate = returnDate;
+    }
+
+    public void setAirline(String airline)
+    {
+        this.airline = airline;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
+    public String getStartDate()
+    {
+        return this.startDate;
+    }
+
+    public String getReturnData()
+    {
+        return this.returnDate;
+    }
+
+    public String getAirline()
+    {
+        return this.airline;
+    }
+
+    public double getCost()
+    {
+        return this.cost;
+    }
+}
