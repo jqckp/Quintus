df3d4a6dde1243ced0bf7467f431e164b07c5633
Jack <porterjp1@appstate.edu>
2023-11-14 18:27:37 -0500

    Inherit from javafx Application class and implement abstract start method

---
+++ src/main/java/edu/appstate/cs/quintus/UI.java
@@ -0,0 +1,18 @@
+package edu.appstate.cs.quintus;
+
+import javafx.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UI extends Application {
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+    
+
+
+
+}
