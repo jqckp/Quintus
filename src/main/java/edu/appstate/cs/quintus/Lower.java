d54d4f073586cb6c506de1ca0ac35b785c018dba
Jack <porterjp1@appstate.edu>
2023-11-16 15:45:52 -0500

    Add mergeSortFlights to sort linked list of flights and replace lowerFirst selection sort method

--- src/main/java/edu/appstate/cs/quintus/Lower.java
+++ src/main/java/edu/appstate/cs/quintus/Lower.java
@@ -1,4 +1,6 @@
 package edu.appstate.cs.quintus;
+
+
 /**
  * This class contains the method for finding the lowest value
  * between two doubles. 
@@ -6,6 +8,13 @@ package edu.appstate.cs.quintus;
  * @author Andrew Tuprin
  * @version 11/2/2023
  */
+
+import java.util.LinkedList;
+import java.util.*;
+
+
+
+
 public class Lower 
 {
     /**
@@ -59,4 +68,135 @@ public class Lower
 
         return narr;
     }
+
+
+
+    
+
+    /**
+     * Merge sort method to sort a linked list of flights by price in ascending order.
+     * 
+     * @author - Jack Porter
+     * @param flights - List of flights to sort.
+     */
+    public static void mergeSortFlights(LinkedList<Flight> flights)
+    {
+
+        int flightsIndex = 0;
+        
+        
+        int length = flights.size();
+
+        if (length < 2)
+        {
+            return;
+        }
+
+        int middle = length / 2;
+
+
+        LinkedList<Flight> leftSub = new LinkedList<>(flights.subList(0, middle));
+        LinkedList<Flight> rightSub = new LinkedList<>(flights.subList(middle, length));
+        
+        mergeSortFlights(leftSub);
+        mergeSortFlights(rightSub);
+
+
+        flightsIndex = merge(leftSub, rightSub, flights, flightsIndex);
+
+    }
+
+    /**
+     * Helper method to merge split linked lists back together.
+     * 
+     * 
+     * @author - Jack Porter
+     * 
+     * @param leftSub - Left half of the linked list.
+     * @param rightSub - Right half of the linked list.
+     * @param flights - Merge two halves into this linked list.
+     */
+    private static int merge(LinkedList<Flight> leftSub,
+         LinkedList<Flight> rightSub, LinkedList<Flight> flights, int flightsIndex)
+         {
+            Iterator<Flight> leftItr = leftSub.iterator();
+            Iterator<Flight> rightItr = rightSub.iterator();
+            
+            
+            
+            if (leftItr.next() == null)
+            {
+                emptyRight(rightItr, flights, flightsIndex);
+                return flightsIndex;
+            }
+
+            if (rightItr.next() == null)
+            {
+                emptyLeft(leftItr, flights, flightsIndex);
+                return flightsIndex;
+            }
+           
+            
+
+            while (leftItr.hasNext() && rightItr.hasNext())
+            {
+
+                Flight leftElement = leftItr.next();
+           
+           
+                Flight rightElement = rightItr.next();
+
+
+                if (leftElement.getCost() <= rightElement.getCost())
+                {
+                    flights.set(flightsIndex++, leftElement);
+                    leftElement = leftItr.next();
+                    
+                }
+
+                else
+                {
+                    flights.set(flightsIndex++, rightElement);
+                    rightElement = rightItr.next();
+                }
+
+                
+            }
+
+            if (leftItr.hasNext())
+            {
+                emptyLeft(leftItr, flights, flightsIndex);
+            }
+
+            if (rightItr.hasNext())
+            {
+                emptyRight(rightItr, flights, flightsIndex);
+            }
+
+            return flightsIndex;
+
+         }
+
+         private static void emptyRight(Iterator<Flight> rightItr, LinkedList<Flight> flights, Integer flightsIndex)
+         {
+                while (rightItr.hasNext())
+                {
+                    flights.set(flightsIndex, rightItr.next());
+                    flightsIndex++;
+                }
+                
+         }
+
+         private static void emptyLeft(Iterator<Flight> leftItr, LinkedList<Flight> flights, Integer flightsIndex)
+         {
+            while (leftItr.hasNext())
+            {
+                flights.set(flightsIndex, leftItr.next());
+                flightsIndex++;
+
+            }
+            
+         }
+
+
 }
