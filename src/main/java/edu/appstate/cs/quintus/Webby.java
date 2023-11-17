d568f94c2523c31550acd73df073e74baaf42b35
DattasaiSagili <sagilids@appstate.edu>
2023-11-17 14:42:50 -0500

    Implement Selenium and Start Data Scrapping.
    Add Selenium into pom.xml file and create Webby.java to start data scraping.

---
+++ src/main/java/edu/appstate/cs/quintus/Webby.java
@@ -0,0 +1,42 @@
+package edu.appstate.cs.quintus;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import java.util.*;
+
+
+public class Webby 
+{
+    
+    public static String startLocation = "RDU";
+    public static String endLocation = "DEL";
+    public static String startDate = "2023-12-17";
+    public static String endDate = "2023-12-24";
+    public static void main(String[] args)
+    {
+        //String url = "https://www.kayak.com/flights/" + startLocation + "-" + endLocation + "/" + startDate + "/" + endDate  + "?sort=bestflight_a";
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/documentation/");     
+    }
+
+    public String getStartLocation()
+    {
+        return startLocation;
+    }
+
+    public String getEndLocation()
+    {
+        return endLocation;
+    }
+
+    public String getStartDate()
+    {
+        return startDate;
+    }
+
+    public String getEndDate()
+    {
+        return endDate;
+    }
+    
+}
