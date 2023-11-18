e301a22ece683e8fcd9ca60468bd988be4693c07
Jack <porterjp1@appstate.edu>
2023-11-17 20:38:28 -0500

    Write code for basic stage with gray scene and set title bar logo to quintus logo

--- src/main/java/edu/appstate/cs/quintus/UI.java
+++ src/main/java/edu/appstate/cs/quintus/UI.java
@@ -10,9 +10,22 @@ import javafx.event.EventHandler;
 
 public class UI  {
 
-    public UI()
+    public UI(Stage stage)
     {
-        
+        stage.setTitle("Quintus");
+
+        stage.getIcons().add(new javafx.scene.image.Image(
+            getClass().getResourceAsStream("Quintus_Logo.png")));
+
+        StackPane root = new StackPane();
+
+        root.setStyle("-fx-background-color: #333232;");
+
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setScene(scene);
+
+        stage.show();
     }
     
 
