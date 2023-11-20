a8587561562a69d3eefcc4b5264911c3597ec1eb
Jack <porterjp1@appstate.edu>
2023-11-20 12:08:33 -0500

    Move file reading logic to readFlights method in Utility class

--- src/main/java/edu/appstate/cs/quintus/App.java
+++ src/main/java/edu/appstate/cs/quintus/App.java
@@ -16,80 +16,10 @@ public class App extends Application
    {
         launch(args);
 
+        Utility.readFlights("flights_data.txt");
 
 
-        try
-        {
-            Input input = new Input();
-            
-
-            File file = new File("flight_data.txt");
-            
-            Scanner s = new Scanner(file);
-
-            String dataValue;
-            String[] arr;
-            LinkedList<Flight> flights = new LinkedList<>();
-
-            while (s.hasNextLine())
-            {
-                dataValue = s.nextLine();
-                arr = dataValue.split(" ");
-                flights.add(new Flight(arr[0], arr[1], arr[2], Double.parseDouble(arr[3])));
-            }
-            
-            // This is spagetti code becasue of this V
-            // System.out.println(Lower.lowestFirst(flights)[0].toString());
-            //System.out.println(Lower.lowestFirst(flights)[1].toString());
-            //System.out.println(Lower.lowestFirst(flights)[2].toString());
-            //System.out.println(Lower.lowestFirst(flights)[69].toString());
-            // We don't know why, but we start at one. - Sai, Oscar Andrew
-            // We fixed it :), for memories - Sai, Oscar Andrew
-
-
-            
-
-            //Lower.mergeSortFlights(flights);
-
-            int i = 0;
-            for (Flight flight : flights)
-            {
-                System.out.println(flight);
-                i++;
-            }
-
-            System.out.println(i);
-
-            System.exit(0);
-            
-
-            Iterator<Flight> itr = flights.iterator();
-
-
-            while (itr.hasNext())
-            {
-                Flight flight = itr.next();
-
-                if(input.getStartDate().equals(flight.getStartDate()) &&
-                    input.getEndDate().equals(flight.getReturnDate())
-                         && Double.parseDouble(input.getCost()) > flight.getCost())
-                {         
-                    System.out.println(flight.toString());
-                }
-                
-            }
-            
-            s.close();
-        }
-
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        catch (ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("Error: Index out of bounds");
-        }
+        
     }
 
 @Override
@@ -97,6 +27,5 @@ public void start(Stage stage) throws Exception
 {
     UI userInterface = new UI(stage);
 
-    stage.show();
 }
 }
