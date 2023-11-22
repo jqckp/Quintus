0b6ed3e35ea47e8e79ce8dd9973d571a2717225d
Oscar Silva-Santiago <oscarsilva032104@gmail.com>
2023-11-22 16:37:33 -0500

    Webby class botton click demo

--- src/main/java/edu/appstate/cs/quintus/Webby.java
+++ src/main/java/edu/appstate/cs/quintus/Webby.java
@@ -1,22 +1,45 @@
 package edu.appstate.cs.quintus;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import java.util.*;
 
 
 public class Webby 
 {
     
-    public static String startLocation = "RDU";
-    public static String endLocation = "DEL";
-    public static String startDate = "2023-12-17";
-    public static String endDate = "2023-12-24";
+    private static String startLocation = "RDU";
+    private static String endLocation = "DEL";
+    private static String startDate = "2023-12-17";
+    private static String endDate = "2023-12-24";
     public static void main(String[] args)
     {
-        //String url = "https://www.kayak.com/flights/" + startLocation + "-" + endLocation + "/" + startDate + "/" + endDate  + "?sort=bestflight_a";
+
+        String url = "https://www.kayak.com/flights/" + startLocation + "-" + endLocation + "/" + startDate + "/" + endDate  + "?sort=bestflight_a";
+        System.out.println(url);
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/documentation/");     
+        driver.get(url);
+        try 
+        {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+        WebElement button = driver.findElement(By.className("bBPb-close"));
+        button.click();
+        try 
+        {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+        
+        driver.close();
+        driver.quit();
+        
     }
 
     public String getStartLocation()
