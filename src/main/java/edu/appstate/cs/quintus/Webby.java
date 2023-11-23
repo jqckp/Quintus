ad2b51116f573c387d2fd8dc7c44f2afc6d4d58a
Oscar Silva-Santiago <oscarsilva032104@gmail.com>
2023-11-23 13:40:40 -0500

    RR problem

--- src/main/java/edu/appstate/cs/quintus/Webby.java
+++ src/main/java/edu/appstate/cs/quintus/Webby.java
@@ -13,17 +13,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Webby 
 {
     
-    private static String startLocation = "RDU";
-    private static String endLocation = "DEL";
-    private static String startDate = "2023-12-17";
-    private static String endDate = "2023-12-24";
-    public static void main(String[] args)
-    {
+    private String startLocation;
+    private String endLocation;
+    private String startDate;
+    private String endDate;
 
-        String url = "https://www.kayak.com/flights/" + startLocation + "-" 
-                    + endLocation + "/" + startDate + "/" + endDate  + "?sort=bestflight_a";
+    public static void main(String[] args) {
+        Webby webby = new Webby("RDU", "DEL", "2023-12-17", "2023-12-24");
+        webby.webbyGo();
+    }
+    
+    public Webby(String startLocation, String endLocation, String startDate, String endDate)
+    {
+        setStartLocation(startLocation);
+        setEndLocation(endLocation);
+        setStartDate(startDate);
+        setEndDate(endDate);
+        
+    }
+
+    public void webbyGo()
+    {
+        String url = "https://www.kayak.com/flights/" + getStartLocation() + "-" 
+                    + getEndLocation() + "/" + getStartDate() + "/" + getEndDate()  + "?sort=bestflight_a";
         System.out.println(url);
         WebDriver driver = new ChromeDriver();
+        driver.get(urll);
+        WebElement pbutton = driver.findElement(By.className("ytp-play-button"));
+        pbutton.click();
+        try 
+        {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
         driver.get(url);
         try 
         {
@@ -49,9 +73,11 @@ public class Webby
         }
         
         List<WebElement> flights = driver.findElements(By.className("nrc6-wrapper"));
+        int count = 0;
         for(WebElement webE: flights)
         {
             //System.out.println(webE);
+            count++;
             String outerHTML = webE.getAttribute("outerHTML");
             Document doc = Jsoup.parse(outerHTML);
             Elements price = doc.getElementsByClass("f8F1-price-text");
@@ -59,30 +85,50 @@ public class Webby
             System.out.println(airline.text());
             System.out.println(price.text());
         }
-        
+        System.out.println(count);;
         driver.quit();
-        
     }
 
     public String getStartLocation()
     {
-        return startLocation;
+        return this.startLocation;
     }
 
     public String getEndLocation()
     {
-        return endLocation;
+        return this.endLocation;
     }
 
     public String getStartDate()
     {
-        return startDate;
+        return this.startDate;
     }
 
     public String getEndDate()
     {
-        return endDate;
+        return this.endDate;
     }
     
+    public void setStartLocation(String startLocation)
+    {
+        this.startLocation = startLocation;
+    }
+
+    public void setEndLocation(String endLocation)
+    {
+        this.endLocation = endLocation;
+    }
+
+    public void setStartDate(String startDate)
+    {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(String endDate)
+    {
+        this.endDate = endDate;
+    }
+
+    private String urll = "https://youtu.be/dQw4w9WgXcQ?si=HWGsV2IEMpB-dLhQ";
 }
 
