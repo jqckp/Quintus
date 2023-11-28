75e418bb0faaf3d833654646e5390efe319408ce
Jack <porterjp1@appstate.edu>
2023-11-28 15:54:09 -0500

    Remove everything but UI initilization

--- src/main/java/edu/appstate/cs/quintus/App.java
+++ src/main/java/edu/appstate/cs/quintus/App.java
@@ -16,13 +16,9 @@ public class App extends Application
     {
         launch(args);
 
-<<<<<<< HEAD
         //Utility.readFlights("flights_data.txt");
 
 
-=======
-        Utility.readFlights("flights_data.txt");
->>>>>>> bd385e61d4b0e54b071754d1f9e6c81388a15a75
         
     }
 
