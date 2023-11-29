0afb3f52e3d9e177507a5246c5a7a63357be1f27
Oscar Silva-Santiago <oscarrsilvaa04@gmail.com>
2023-11-29 15:48:41 -0500

    App class clean up and warning suppresion

--- src/main/java/edu/appstate/cs/quintus/App.java
+++ src/main/java/edu/appstate/cs/quintus/App.java
@@ -1,6 +1,5 @@
 package edu.appstate.cs.quintus;
 
-import java.util.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,21 +11,11 @@ public class App extends Application
     */   
     public static void main(String[] args)
     {
-     
         launch(args);
-        /* 
-        LinkedList<Flight> flights = new LinkedList<Flight>();
-        Input input = new Input();
-        Webby webby = new Webby(input.getStartLocation(), input.getEndLocation(), input.getStartDate(), input.getEndDate());
-        webby.webbyGo(flights);
-        Utility.readFlights(flights, input);
-        */
-
-        
-
     }
 
-@Override
+    @SuppressWarnings(value = { "unused" })
+    @Override
     public void start(Stage stage) throws Exception 
     {
         UI userInterface = new UI(stage);
