081c24fcd313bf11cf755a598cf002ad31df234b
Oscar Silva-Santiago <oscarrsilvaa04@gmail.com>
2023-12-02 14:37:27 -0500

    App.java testing date

--- src/main/java/edu/appstate/cs/quintus/App.java
+++ src/main/java/edu/appstate/cs/quintus/App.java
@@ -1,6 +1,10 @@
 package edu.appstate.cs.quintus;
 
+import java.util.Calendar;
 import java.util.LinkedList;
+
+import org.openqa.selenium.devtools.v117.runtime.model.CallArgument;
+
 import java.awt.Desktop;
 import java.net.URI;
 import javafx.application.Application;
@@ -20,7 +24,39 @@ public class App extends Application
         Input input = new Input();
         input.setInput("2023-12-14", "10000", "CLT", "NYC");
         Webby webby = new Webby(input.getStartLocation(), input.getEndLocation(), input.getStartDate(), input.getEndDate());
-        webby.webbyOneAirline(flights);
+        //webby.webbyOneAirline(flights);
+
+        int duration = 0;
+
+        Calendar earliest = Calendar.getInstance();
+        earliest.set(2023, 11, 13);
+        Calendar lateist = Calendar.getInstance();
+        lateist.set(2023, 11, 24);
+        Calendar durAdd = Calendar.getInstance();
+
+        String date;
+        int year;
+        int month;
+        int day;
+
+
+        earliest.add(Calendar.DAY_OF_MONTH, duration);
+        if (duration == 0)
+        {
+            year = earliest.get(Calendar.YEAR);
+            month = earliest.get(Calendar.MONTH);
+            day = earliest.get(Calendar.DAY_OF_MONTH);
+            webby.webbyOneAirline(flights);
+            earliest.add(Calendar.DAY_OF_MONTH, 1);
+            input.setInput(STYLESHEET_CASPIAN, STYLESHEET_CASPIAN, STYLESHEET_MODENA, STYLESHEET_CASPIAN);
+
+        }
+        else
+        {
+            webby.webbyTwoAirline(flights);
+            earliest.add(Calendar.DAY_OF_MONTH, 1);
+            durAdd.add(Calendar.DAY_OF_MONTH, 1);
+        }
         Utility.readOneFlights(flights, input);
         Desktop desktop = Desktop.getDesktop();
         desktop.browse(new URI(flights.get(0).getUrl()));
